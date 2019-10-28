1d74f034e20b918cbc1a89de63d5c905c2e42e5f
Julien Tauran <julien.tauran@epitech.eu>
2019-10-28 15:37:45 +0100

    Added checklength

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,27 @@
 module Main where
 
 import Lib
+import System.Console.GetOpt
+import System.IO
+import System.Exit
+import System.Environment
+import Data.List
+import Data.Char
+import Control.Monad
+import Text.Printf
+
+checkLength :: Int List -> String
+checkLength size list   | size == 1 = list !! 0
+                        | otherwise = ""
 
 main :: IO ()
 main = do
-    putStrLn "hello world"
-    -- args <- getArgs
 
-    -- let len [] = 0
-    -- len (h:t) = 1 + len t
+    args <- getArgs
+    -- case 
+    -- str <- return( args !! 0)
+    putStrLn ("The first arg is " $ checkLength length args, args)
 
-    -- case len args of
-    --     1 -> do
-    --         putStrLn "1 argument"
-    --     2 -> do
-    --         putStrLn "2 argument"
-    --     _ -> do
-    --             putStrLn "Too many arguments"
-    --             return()
+-- checkLength :: (List list, Integral size) => list => size -> String
+-- checkLength 1 = "One!"
+-- checkLength x = "Not between 1 and 5"
