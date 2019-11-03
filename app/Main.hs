87d13777a2ac5092191d6cdb6034e1dbd3b0bd6b
Julien Tauran <julient@julient.home>
2019-11-03 14:37:30 +0100

    Fixed the div by zero

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,36 @@ module Main where
 import Calculator
 import System.Environment
 import Text.Printf
+import Control.Exception
+import System.Exit
+
+catchAny :: IO a -> (SomeException -> IO a) -> IO a
+catchAny = Control.Exception.catch
+
+checkChar :: Char -> Bool
+checkChar x
+        | x == '+' = True
+        | x == '-' = True
+        | x == '*' = True
+        | x == '/' = True
+        | x == '^' = True
+        | x == '%' = True
+        | x == '(' = True
+        | x == ')' = True
+        | x == '.' = True
+        | x == ' ' = True
+        | x == '\t' = True
+        | x `elem` ['0'..'9'] = True
+        | otherwise           = False
+
+dangerous :: String -> IO Double
+dangerous str = do
+                if (((all checkChar str) == False) || (evalExpr str) < -2000000000000000000) then error "error" else return (evalExpr str)
 
 main :: IO ()
 main = do
     args <- getArgs
-
-    case args of
-        [] -> putStrLn "Not enough arguments"
-        [str] -> (putStrLn $ (printf "%.2f" $ (evalExpr (args!!0))))
-        otherwise -> putStrLn "Too many arguments"
+    result <- catchAny (dangerous (args!!0))$ \e -> do
+        putStrLn $ "Got an exception: " ++ show e
+        exitWith (ExitFailure 84)
+    (putStrLn $ (printf "%.2f" $ (result)))
